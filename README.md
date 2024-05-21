35768244c9e88179a2e330863ea5fc53bb454a93
Jen Guerra <southpawgeek@users.noreply.github.com>
2024-05-21 00:21:30 -0400

    initial version

---
+++ README.md
@@ -0,0 +1,8 @@
+# Scyllaphonia
+
+Removes Scylla's vocals during the Oceanus boss fight.
+
+## Features
+
+- Prevent shipwrecks by muting Scylla's singing.
+
---
+++ src/main.lua
@@ -0,0 +1,55 @@
+-- grabbing our dependencies,
+-- these funky (---@) comments are just there
+--	 to help VS Code find the definitions of things
+
+---@diagnostic disable-next-line: undefined-global
+local mods = rom.mods
+
+---@module 'SGG_Modding-ENVY-auto'
+mods['SGG_Modding-ENVY'].auto()
+-- ^ this gives us `public` and `import`, among others
+--	and makes all globals we define private to this plugin.
+---@diagnostic disable: lowercase-global
+
+---@diagnostic disable-next-line: undefined-global
+rom = rom
+---@diagnostic disable-next-line: undefined-global
+_PLUGIN = PLUGIN
+
+---@module 'SGG_Modding-Hades2GameDef-Globals'
+game = rom.game
+
+---@module 'SGG_Modding-ModUtil'
+modutil = mods['SGG_Modding-ModUtil']
+
+---@module 'SGG_Modding-Chalk'
+chalk = mods["SGG_Modding-Chalk"]
+---@module 'SGG_Modding-ReLoad'
+reload = mods['SGG_Modding-ReLoad']
+
+---@module 'config'
+config = chalk.auto()
+-- ^ this updates our config.toml in the config folder!
+public.config = config -- so other mods can access our config
+
+local function on_ready()
+    -- what to do when we are ready, but not re-do on reload.
+    if config.enabled == false then return end
+
+    modutil.mod.Path.Wrap("StartBossRoomMusic", function(base)
+        if game.CurrentRun.CurrentRoom.Name ~= "G_Boss01" then
+            base()
+        end
+    end)
+end
+
+local function on_reload()
+end
+
+-- this allows us to limit certain functions to not be reloaded.
+local loader = reload.auto_single()
+
+-- this runs only when modutil and the game's lua is ready
+modutil.on_ready_final(function()
+    loader.load(on_ready, on_reload)
+end)
